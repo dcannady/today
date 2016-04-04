955a20db4c8fc3589fc90cf960c455a557aa7ed2
David Cannady <David Cannady>
2016-04-04 12:34:25 -0700

    oddNumbers

---
+++ Checkboard.php
@@ -0,0 +1,46 @@
+<?php
+ $name ="David Cannady";
+ echo "Hello, $name";
+?>
+
+$board = [
+  [2,2,2,2,2,2,2,2]
+  [2,1,1,1,1,2,2,2]
+  [1,2,2,1,2,1,2,1]
+  [2,1,1,2,2,1,1,2]
+  [1,2,1,1,1,2,1,2]
+  [2,1,1,2,1,2,2,1]
+  [2,1,2,1,2,2,1,2]
+  [2,2,2,2,1,1,2,1]
+?>
+<table>
+    <?php
+    function blackFirst(){
+      echo "<tr>";
+      for($i=0; $i<8; $i++){
+        if((i=0; $1 % 2 ==0){
+          echo "<td id= 'black'></td>";      }
+        } else {
+          echo "<td id='white' ></td>";
+        }
+      }
+      echo <"/tr>";
+    }
+    function whiteFirst(){
+      echo "<tr>";
+      for($i=0; $i<8; $i++){
+        if($i % 2 ==0){
+          echo "<td id= 'white'></td>";      }
+        } else {
+          echo "<td id='black' ></td>";
+        }
+      }
+      echo <"/tr>";
+    }
+      for($k=0; $k<8; $k++){
+        if ($k % 2 ==0){
+          blackFirst();
+        } else {
+
+        }
+      }
---
+++ oddNumbers.php
@@ -0,0 +1,9 @@
+<?php
+  function oddNumbers(){
+    for ($i=1; $i<1000; $i+=2){
+      echo $i;
+    }
+  }
+  oddNumbers ();
+ ?>
+ 
